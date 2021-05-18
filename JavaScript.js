211c2b8cf893ef2abb8105fec9fb9229b9bfadae
zakiabdessem <zakiabdessemed35@gmail.com>
2021-05-18 21:42:50 +0100

    Update and rename README.md to JavaScript.js

---
+++ JavaScript.js
@@ -0,0 +1,28 @@
+var a=prompt("Enter Number");
+var b=prompt("Enter Number");
+if (a=b) {
+    document.getElementById("Y").innerHTML ="A = B";
+    break;
+}
+else if (a<b) {
+    document.getElementById("Y").innerHTML ="A < B";
+    break;
+}
+else if (a>b) {
+    document.getElementById("Y").innerHTML ="A > B";
+    break;
+}  
+var a=prompt("Enter Number");
+var b=prompt("Enter Number");
+if (a=b) {
+    document.getElementById("Y").innerHTML ="A = B";
+    break;
+}
+else if (a<b) {
+    document.getElementById("Y").innerHTML ="A < B";
+    break;
+}
+else if (a>b) {
+    document.getElementById("Y").innerHTML ="A > B";
+    break;
+}  
