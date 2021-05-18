1c63a5749f36bbaeb7e042fdcc8ae0ff5440c416
zakiabdessem <zakiabdessemed35@gmail.com>
2021-05-18 21:44:17 +0100

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,19 +1,6 @@
 var a=prompt("Enter Number");
 var b=prompt("Enter Number");
-if (a=b) {
-    document.getElementById("Y").innerHTML ="A = B";
-    break;
-}
-else if (a<b) {
-    document.getElementById("Y").innerHTML ="A < B";
-    break;
-}
-else if (a>b) {
-    document.getElementById("Y").innerHTML ="A > B";
-    break;
-}  
-var a=prompt("Enter Number");
-var b=prompt("Enter Number");
+
 if (a=b) {
     document.getElementById("Y").innerHTML ="A = B";
     break;
